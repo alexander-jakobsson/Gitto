2b515ccf221b3f794a4b482007d9022ab150a1a1
Alexander Jakobsson <jakobsson.alex@gmail.com>
2017-11-15 16:18:59 +0100

    Add skeleton to RPNCalculator.Java.

--- src/RPNCalculator.java
+++ src/RPNCalculator.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class RPNCalculator {
+    private Boolean continueLoop = true;
+    private List<String> calculatorStack = new ArrayList<>();
+    private Scanner lineReader = new Scanner( System.in );
+    private String currentLine;
+
+    public void REPL() {
+
+        while (continueLoop) {
+
+            currentLine = lineReader.next();
+            System.out.println("Current line is: " + currentLine);
+
+        }
+
+    }
+
 }
+
